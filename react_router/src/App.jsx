28bdfdbb51afc2711bb0b0fd65608753ba286f93
SunilSA24 <krishnasa464@gmail.com>
2025-05-21 00:29:48 +0530

    added router

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './Components/Home'
+import About from './Components/About'
+import User from './Components/User'
+import Notfound from './Components/Notfound'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/users' element={<User />}></Route>
+      <Route path='*' element={<Notfound />}></Route>
+    </Routes>
+
+    <nav>
+      <ul>
+        <li>
+           <Link to="/">Home</Link>
+        </li>
+        <li>
+           <Link to="/about">About</Link>
+        </li>
+        <li>
+           <Link to="/users">User</Link>
+        </li>
+
+      </ul>
+    </nav>
     </>
   )
 }
